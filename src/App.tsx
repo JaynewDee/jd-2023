f0d53e21c2bd2bfa56a5670da39bea232f83d648
NuminousBlue <jdiehl2236@gmail.com>
2023-03-20 20:26:23 -0500

    extract footnote into component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
+import Footnote from "./components/Footnote";
 import Portal from "./Portal";
 
 function App() {
   return (
     <div className="App">
       <Portal />
-      <p className="footnote">
-        Website built and maintained by &copy;2023 Joshua Newell Diehl
-      </p>
+      <Footnote />
     </div>
   );
 }
---
+++ src/components/Footnote.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footnote = () => {
+  return (
+    <div className="footnote">
+      <p>built and maintained by</p>
+      <p>Joshua Newell Diehl</p>
+      <p>&copy;2023</p>
+    </div>
+  );
+};
+
+export default Footnote;
