c42dfe4137c218d714fbeeb9b351c4aaef7cdaa2
NuminousBlue <jdiehl2236@gmail.com>
2023-03-28 16:31:50 -0500

    skills icons

--- src/components/categories/Skills.tsx
+++ src/components/categories/Skills.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { DisplayProps } from "../../Portal";
-import { TbClipboardCheck as List } from "react-icons/tb";
+import { TbClipboardCheck as List, TbFileArrowLeft } from "react-icons/tb";
 import IconTitle from "../partials/IconTitle";
 import { IconType } from "react-icons";
-import { SiJavascript, SiPython } from "react-icons/si";
-
+import { SiJavascript, SiPython, SiRust } from "react-icons/si";
+import { TiArrowLeft, TiArrowRight } from "react-icons/ti";
 //
 
 type SkillType = {
@@ -17,22 +17,37 @@ const skillsData: SkillType[] = [
   {
     name: "JavaScript",
     Icon: SiJavascript,
-    level: 7
+    level: 8
   },
   {
     name: "Python",
     Icon: SiPython,
     level: 5
+  },
+  {
+    name: "Rust",
+    Icon: SiRust,
+    level: 5
   }
 ];
 
 const Skill = (name: string, Icon: IconType, level: number, key: number) => (
-  <div className="skill-box" key={key}>
+  <div className="skill-box" key={key} style={{ width: `15%` }}>
     <div className="level-bar-outline">
       <div className="level-bar" style={{ height: `${level * 2}vh` }}></div>
     </div>
-    <span>{name}</span>
-    <span>{Icon({})}</span>
+    <span>{Icon({ size: "1.33rem" })}</span>
+    <span
+      style={{
+        textAlign: "center",
+        writingMode: "vertical-lr",
+        position: "absolute",
+        transform: "rotate(180deg)",
+        marginBottom: ""
+      }}
+    >
+      {name}
+    </span>
   </div>
 );
 
@@ -42,6 +57,11 @@ const Skills: React.FC<DisplayProps> = ({ backBtn }) => {
       <div className="category-header">
         {backBtn()}
         {IconTitle("SKILLS", List)}
+      </div>
+      <div className="skill-switch-header">
+        <button className="category-back-btn">{TiArrowLeft({})}</button>
+        <h3>Programming</h3>
+        <button className="category-next-btn">{TiArrowRight({})}</button>
       </div>
       <div className="skills-container">
         {skillsData.map(({ name, Icon, level }, idx) =>
