ce08fd5b535fe5fd007a92d07948abcb2b34f3a8
NuminousBlue <jdiehl2236@gmail.com>
2023-08-25 19:27:45 -0500

    implement assets compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compress from "vite-plugin-compress";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compress()],
   assetsInclude: ["**/*.jpg", "./src/assets/*.pdf"],
 });
