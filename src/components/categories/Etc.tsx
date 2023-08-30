29405e88b645802c6d7b3536eac66a6b1743bbe5
NuminousBlue <jdiehl2236@gmail.com>
2023-08-30 17:47:03 -0500

    add feedback entry

--- src/components/categories/Etc.tsx
+++ src/components/categories/Etc.tsx
@@ -23,6 +23,7 @@ const feedbacks = [
   "I would like to shoutout Josh our TA he's been great helping out my group with the project.  He's tremendously patient and is always willing to help us work through our issue.",
   "My TA Josh was extremely helpful not in only writing code, but in helping with motivation and the feeling of being completely lost/behind with some subjects and helped me get a better perspective and outlook on this week's project and the course in general.",
   "Josh and Chris were amazing in taking the complex structure of Javascript and breaking it down into simple terms.",
+  "Just wanted to give Josh the TA another shoutout. I believe he's the role model software developer. He's been incredibly informative and encouraging during our Project 3.",
   "TAs Chris ***** and Joshua Diehl were incredibly helpful this week and took time beyond office hours to help in every way possible.  Very thankful for them!!",
   "Joshua was very patient helping answer my questions.  As we were problem solving he helped me understand what the computer was saying to me as I sent commands to it.",
   "Joshua Diehl has been very kind and helpful and communicated well!"
